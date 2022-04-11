e85a13914072a0b134e660465482148a0b1e0c35
W-Kraxcker <86090853+W-Kraxcker@users.noreply.github.com>
2022-04-12 00:52:44 +0700

    Add files via upload

---
+++ typed.js
@@ -0,0 +1,10 @@
+new Typed('#typed',{
+    strings : ['Hacking','Designing','Programming','Coding','Learning'],
+    typeSpeed : 120,
+    delaySpeed : 2000,
+    backsSpeed : 400,
+    cursorChar : '_',
+    shuffle : true,
+    smartBackspace : false,
+    loop : true
+  });
